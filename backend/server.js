360dcc232f5a3b88353d94b65522715f8ab4d5f0
Trevor Simonson <simonsot@gmatc.matc.edu>
2024-09-25 18:48:03 -0500

    model complete

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import {connectDB }from "./config/db.js"
+import {connectDB } from "./config/db.js"
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res)=>{
+app.post("/products", (req, res)=>{
     
 });
 
